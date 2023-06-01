4271f6f58f69ea7b89ea84aadd458ea5021e81c1
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2023-06-01 07:32:29 -0700

    Add tests for code snippets (#8)
    * generate tests for snippets
    
    * updating snippet execution
    
    * grrr
    
    * fixing stuff
    
    * it works
    
    * update snippets
    
    * fix demo
    
    * add test to ci
    
    * document

--- README.md
+++ README.md
@@ -27,6 +27,21 @@ docker run -d -p 3000:3000 documentation
 
 Then navigate to localhost:3000 in the browser.
 
+## Tests
+
+Simply run `npm run test`.
+
+The tests include a compiler check for all code snippets written in .mdx files. These tests will automatically extract
+the relevant snippets into separate files under the `.code_tests` folder (ignored by git) and then try to build each one.
+The files are named per the file path to the code snippet.
+These files remain after the tests complete, which allows you to examine them to help debug the error in case a test fails.
+
+You can also run the code tests for a specific mdx file using:
+
+```
+npm run test -- -t get-price.mdx
+```
+
 ## License
 
 This project is derived from the [Nextra Docs Template](https://github.com/shuding/nextra-docs-template).
---
+++ __tests__/validateCodeSnippets.ts
@@ -0,0 +1,136 @@
+import fs from "fs";
+import { join } from "path";
+import { exec, ExecException } from "child_process";
+import { promisify } from "util";
+import { tmpdir } from "os";
+
+const codeSnippetRegex = /```([a-zA-Z]+)[\s\S]*?```/g;
+const codeSnippetsDir = ".code_tests";
+
+const execPromise = promisify(exec);
+
+function extractCodeSnippetsFromFile(filePath: string): string[] {
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const codeSnippets = fileContent.match(codeSnippetRegex) || [];
+  return codeSnippets.map((snippet) => snippet.replace(/```/g, "").trim());
+}
+
+function wrapTsCodeInAsyncFunction(code: string): string {
+  const importStatementsRegex = /^(\s*import\s+.*;\s*)+/gm;
+  const importStatementsMatch = code.match(importStatementsRegex);
+
+  if (importStatementsMatch) {
+    const importStatements = importStatementsMatch.join("\n");
+    const coreLogic = code.replace(importStatementsRegex, "").trim();
+
+    const wrappedCode = `
+      ${importStatements}
+
+      (async () => {
+        ${coreLogic}
+      })();
+    `;
+
+    return wrappedCode;
+  }
+
+  return code;
+}
+
+function wrapSolCode(code: string): string {
+  const importStatementsRegex = /^(\s*import\s+.*;\s*)+/gm;
+  const importStatementsMatch = code.match(importStatementsRegex);
+
+  if (importStatementsMatch) {
+    const importStatements = importStatementsMatch.join("\n");
+    const coreLogic = code.replace(importStatementsRegex, "").trim();
+
+    const wrappedCode = `
+      // SPDX-License-Identifier: UNLICENSED
+      pragma solidity ^0.8.20;
+      ${importStatements}
+
+      contract ExampleContract {
+         function wrapper() public {
+           ${coreLogic}
+         }
+      }
+    `;
+
+    return wrappedCode;
+  }
+
+  return code;
+}
+
+async function runCodeSnippet(
+  code: string,
+  language: string,
+  id: string
+): Promise<[boolean, string]> {
+  let command: string;
+
+  if (language === "typescript") {
+    const tempFilePath = join(codeSnippetsDir, `${id}.ts`);
+
+    fs.writeFileSync(tempFilePath, wrapTsCodeInAsyncFunction(code), "utf8");
+    command = `npx tsc --target es5 --module esnext  --esModuleInterop --moduleResolution node --skipLibCheck --noEmit ${tempFilePath}`;
+  } else if (language === "solidity") {
+    const tempFilePath = join(codeSnippetsDir, `${id}.sol`);
+    fs.writeFileSync(tempFilePath, wrapSolCode(code), "utf8");
+    command = `npx solc --base-path . --include-path node_modules/\\@pythnetwork --output-dir ${tmpdir()} --bin ${tempFilePath}`;
+  } else {
+    return [false, `Unsupported language: ${language}`];
+  }
+
+  try {
+    const result = await execPromise(command);
+    return [true, JSON.stringify(result.stdout)];
+  } catch (e: any) {
+    const errorMsg = `Error running command ${e.cmd}\n--stdout--\n${e.stdout}\n\n--stderr--\n${e.stderr}`;
+    return [false, errorMsg];
+  }
+}
+
+function clearCodeSnippetsDir(): void {
+  if (fs.existsSync(codeSnippetsDir)) {
+    fs.rmSync(codeSnippetsDir, { recursive: true, force: true });
+  }
+  fs.mkdirSync(codeSnippetsDir);
+}
+
+function validateCodeSnippets(directoryPath: string): void {
+  clearCodeSnippetsDir();
+
+  const files = fs.readdirSync(directoryPath);
+
+  files.forEach((file) => {
+    const fullPath = `${directoryPath}/${file}`;
+    const stats = fs.statSync(fullPath);
+
+    if (stats.isDirectory()) {
+      validateCodeSnippets(fullPath);
+    } else if (stats.isFile() && file.endsWith(".mdx")) {
+      const codeSnippets = extractCodeSnippetsFromFile(fullPath);
+      codeSnippets.forEach((snippet, index) => {
+        const language = snippet.split("\n")[0].trim();
+        const code = snippet.slice(language.length).trim();
+        const id = `${fullPath
+          .replace(new RegExp("^./*"), "")
+          .replaceAll("/", "-")
+          .replaceAll(".", "-")}-${index + 1}`;
+
+        it(`${file} snippet #${index + 1} (${language})`, async () => {
+          const [success, error] = await runCodeSnippet(code, language, id);
+          expect(success, error).toBe(true);
+        });
+      });
+    }
+  });
+}
+
+// Specify the root directory containing the .mdx files
+describe("Validate code snippets", () => {
+  const rootDirectory = "./";
+  validateCodeSnippets(rootDirectory);
+});
