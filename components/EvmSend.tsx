3539db8233a92da59971146f649b8c39ce8b0534
Daniel Chew <cctdaniel@outlook.com>
2023-07-03 16:32:08 +0800

    fix build error

--- components/EvmSend.tsx
+++ components/EvmSend.tsx
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import { useGlobalContext } from "../contexts/GlobalContext";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 interface EvmSendProps {
   functionName: string;
