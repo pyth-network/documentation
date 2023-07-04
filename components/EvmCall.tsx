8105a7de154465079534c03ce97673465a70cc83
Daniel Chew <cctdaniel@outlook.com>
2023-07-04 15:55:35 +0800

    remove connect wallet button for EvmCall

--- components/EvmCall.tsx
+++ components/EvmCall.tsx
@@ -1,5 +1,4 @@
-import detectEthereumProvider from "@metamask/detect-provider";
-import { Result, ethers, isError } from "ethers";
+import { Result, isError } from "ethers";
 import { useEffect, useState } from "react";
 import { useGlobalContext } from "../contexts/GlobalContext";
 import EvmNetworkSelector from "./EvmNetworkSelector";
@@ -21,113 +20,74 @@ interface EvmCallProps {
  * TODO: probably better to pass the contract address / ABI as arguments (?)
  */
 const EvmCall = ({ functionName, buildArguments }: EvmCallProps) => {
-  const [response, setResponse] = useState<string | undefined>(undefined);
+  const [response, setResponse] = useState<string>();
   // The preface is explainer text that shows up before the response itself.
-  const [responsePreface, setResponsePreface] = useState<string>("");
-  const [isStale, setIsStale] = useState<boolean>(false);
+  const [responsePreface, setResponsePreface] = useState<string>();
 
-  const { keyValueStore, pythContract, signer, setSigner, setProvider } =
-    useGlobalContext();
+  const { keyValueStore, pythContract } = useGlobalContext();
 
   useEffect(() => {
-    setIsStale(true);
+    clearResponse();
   }, [keyValueStore, pythContract]);
 
-  const connectWallet = async () => {
-    const ethereumProvider = await detectEthereumProvider();
-
-    if (ethereumProvider) {
-      // @ts-ignore
-      const myProvider = new ethers.BrowserProvider(ethereumProvider);
-      setProvider(myProvider);
-
-      // It also provides an opportunity to request access to write
-      // operations, which will be performed by the private key
-      // that MetaMask manages for the user.
-      const mySigner = await myProvider.getSigner();
-      setSigner(mySigner);
-    } else {
-      alert("Please install MetaMask!");
-    }
-  };
-
   const sendTransaction = async () => {
-    if (signer != undefined) {
-      const args: any[] | undefined = buildArguments(keyValueStore);
+    const args: any[] | undefined = buildArguments(keyValueStore);
 
-      // TODO: validate arguments
-      if (args === undefined || args.includes(undefined)) {
-        setResponse(`Please populate all of the arguments with valid values.`);
-        setIsStale(false);
-      } else {
-        try {
-          const result: Result = await pythContract[
-            functionName
-          ].staticCallResult(...args);
-          const resultString = renderResult(result, "");
+    // TODO: validate arguments
+    if (args === undefined || args.includes(undefined)) {
+      setResponse(`Please populate all of the arguments with valid values.`);
+    } else {
+      try {
+        setResponsePreface("Loading...");
+        const result: Result = await pythContract[
+          functionName
+        ].staticCallResult(...args);
+        const resultString = renderResult(result, "");
 
-          setResponsePreface("EVM call succeeded with result:");
-          setResponse(resultString);
-          setIsStale(false);
-        } catch (error) {
-          if (isError(error, "CALL_EXCEPTION")) {
-            const ethError = pythContract.interface.parseError(error.data);
-            setResponsePreface("EVM call reverted with exception:");
-            setResponse(`${ethError.name}(${renderResult(ethError.args, "")})`);
-            setIsStale(false);
-          } else {
-            setResponsePreface("An unknown error occurred. Error details:");
-            setResponse(error.toString);
-            setIsStale(false);
-          }
+        setResponsePreface("EVM call succeeded with result:");
+        setResponse(resultString);
+      } catch (error) {
+        if (isError(error, "CALL_EXCEPTION")) {
+          const ethError = pythContract.interface.parseError(error.data);
+          setResponsePreface("EVM call reverted with exception:");
+          setResponse(`${ethError.name}(${renderResult(ethError.args, "")})`);
+        } else {
+          setResponsePreface("An unknown error occurred. Error details:");
+          setResponse(error.toString);
         }
       }
-    } else {
-      alert("Please connect your wallet first!");
     }
   };
 
   const clearResponse = async () => {
+    setResponsePreface(undefined);
     setResponse(undefined);
   };
 
   return (
     <div>
       <div className="flex justify-between">
-        {signer !== undefined ? (
-          <>
-            <div className="space-x-2">
-              <button
-                className="bg-[#E6DAFE] text-[#141227] font-normal text-base hover:bg-[#F2ECFF] my-4"
-                onClick={sendTransaction}
-              >
-                execute query
-              </button>
-              <button
-                className="font-normal text-base my-4"
-                onClick={clearResponse}
-              >
-                clear
-              </button>
-            </div>
-            <EvmNetworkSelector />
-          </>
-        ) : (
+        <div className="space-x-2">
           <button
             className="bg-[#E6DAFE] text-[#141227] font-normal text-base hover:bg-[#F2ECFF] my-4"
-            onClick={connectWallet}
+            onClick={sendTransaction}
           >
-            connect wallet
+            execute query
           </button>
-        )}
+          <button
+            className="font-normal text-base my-4"
+            onClick={clearResponse}
+          >
+            clear
+          </button>
+        </div>
+        <EvmNetworkSelector />
       </div>
-      {response !== undefined ? (
-        <div className={"response " + (isStale ? "stale" : "")}>
+      {responsePreface !== undefined && (
+        <div className="response">
           <p>{responsePreface}</p>
           <pre>{response}</pre>
         </div>
-      ) : (
-        <div className={"trial"} />
       )}
     </div>
   );
