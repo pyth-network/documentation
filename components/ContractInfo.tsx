9849824fa71106de23ba43e6c8f6ddc8d3013120
Daniel Chew <cctdaniel@outlook.com>
2023-07-04 14:09:19 +0800

    use JsonRpcProvider instead of BrowserProvider

--- components/ContractInfo.tsx
+++ components/ContractInfo.tsx
@@ -9,54 +9,67 @@ import EvmNetworkSelector from "./EvmNetworkSelector";
 const ContractInfo = () => {
   const { networkConfig, pythContract } = useGlobalContext();
 
-  const [fee, setFee] = useState<string>("loading...");
-  const [validTimePeriod, setValidTimePeriod] = useState<string>("loading...");
+  const [fee, setFee] = useState<string>();
+  const [validTimePeriod, setValidTimePeriod] = useState<string>();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     async function helper() {
       try {
+        setIsLoading(true);
         setFee((await pythContract.getUpdateFee(["0x01"])).toString());
         setValidTimePeriod(
           (await pythContract.getValidTimePeriod()).toString()
         );
+        setIsLoading(false);
       } catch (error: any) {
         console.error(error);
-        setFee("loading...");
-        setValidTimePeriod("loading...");
+        setIsLoading(false);
       }
     }
     helper();
   }, [pythContract]);
 
+  const configValue = (value: string) => {
+    return isLoading ? "Loading..." : value;
+  };
+
   return (
     <div>
       <EvmNetworkSelector />
-      <table className="w-full text-left">
+      <table className="w-full text-left table-fixed">
         <tbody>
           <tr
             key="Contract Address"
             className="border-t border-darkGray5 bg-darkGray border-none"
           >
-            <td className="py-3 pl-6 pr-1 lg:pl-14">Contract Address</td>
-            <td className="py-3 pl-1 lg:pl-14">{networkConfig.pythAddress}</td>
+            <td className="py-3 pl-6 pr-1 lg:pl-14 w-48">Contract Address</td>
+            <td className="py-3 pl-1 lg:pl-14">
+              {configValue(networkConfig.pythAddress)}
+            </td>
           </tr>
           <tr key="Chain ID" className="border-t border-darkGray5 bg-darkGray">
             <td className="py-3 pl-6 pr-1 lg:pl-14">Chain ID</td>
-            <td className="py-3 pl-1 lg:pl-14">{networkConfig.info.chainId}</td>
+            <td className="py-3 pl-1 lg:pl-14">
+              {configValue(networkConfig.info.chainId)}
+            </td>
           </tr>
           <tr
             key="Update Fee"
             className="border-t border-darkGray5 bg-darkGray"
           >
             <td className="py-3 pl-6 pr-1 lg:pl-14">Update Fee</td>
-            <td className="py-3 pl-1 lg:pl-14">{fee}</td>
+            <td className="py-3 pl-1 lg:pl-14">{configValue(fee)}</td>
           </tr>
           <tr
             key="Valid Time Period"
             className="border-t border-darkGray5 bg-darkGray border-none"
           >
             <td className="py-3 pl-6 pr-1 lg:pl-14">Valid Time Period</td>
-            <td className="py-3 pl-1 lg:pl-14">{validTimePeriod}</td>
+            <td className="py-3 pl-1 lg:pl-14">
+              {configValue(validTimePeriod)}
+            </td>
           </tr>
         </tbody>
       </table>
