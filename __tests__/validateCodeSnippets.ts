68f992e9c4d0e1987a4f79656792a8908066bc7f
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2023-07-13 16:57:22 -0700

    aptos reference

--- __tests__/validateCodeSnippets.ts
+++ __tests__/validateCodeSnippets.ts
@@ -143,6 +143,7 @@ function validateCodeSnippets(directoryPath: string): void {
 }
 
 describe("Validate code snippets", () => {
-  // We only validate code snippets in the API reference
+  // We only validate code snippets in the API reference.
+  // However, we exclude Aptos for now because it's annoying (and doesn't seem worth it).
   validateCodeSnippets("./pages/evm");
 });
