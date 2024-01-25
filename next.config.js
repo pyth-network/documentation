fc9e1b34b69fa28e814dcc352a2758956a98082f
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2024-01-25 12:35:33 -0800

    with redirects

--- next.config.js
+++ next.config.js
@@ -4,7 +4,100 @@ const withNextra = require("nextra")({
   latex: true,
 });
 
-const oldDocPaths = [[]];
+// Use this array as a shorter way to specify redirect URLs so we can write down a lot of them.
+const permanentRedirectArray = [
+  // Third version redirects
+  ["/", "/home"],
+  ["/evm", "/price-feeds/use-real-time-data/evm"],
+  ["/aptos", "/price-feeds/use-real-time-data/aptos"],
+  ["/cosmwasm", "/price-feeds/use-real-time-data/cosmwasm"],
+
+  // First version of docs site -> third version
+
+  // Second version of docs site -> third version
+  ["/documentation/metrics/:path*", "/home/metrics/:path*"],
+  [
+    "/documentation/how-pyth-works/:slug*",
+    "/price-feeds/how-pyth-works/:slug*",
+  ],
+  ["/documentation/benchmarks", "/benchmarks"],
+  ["/pyth-token", "/home/pyth-token"],
+  ["/documentation/publish-data/:slug*", "/price-feeds/publish-data/:slug*"],
+  [
+    "/documentation/solana-price-feeds/:slug*",
+    "/price-feeds/solana-price-feeds/:slug*",
+  ],
+  ["/documentation/whitepaper/:slug*", "/price-feeds/whitepaper/:slug*"],
+  ["/documentation/security", "/home/security"],
+
+  ["/documentation/entropy", "/entropy"],
+  ["/documentation/entropy/protocol-design", "/entropy/protocol-design"],
+  ["/documentation/entropy/best-practices", "/entropy/best-practices"],
+  [
+    "/documentation/entropy/solidity-sdk",
+    "/entropy/generate-random-numbers/solidity-sdk",
+  ],
+  [
+    "/documentation/entropy/evm",
+    "/entropy/generate-random-numbers/solidity-sdk",
+  ],
+
+  ["/documentation/pythnet-price-feeds", "/price-feeds/pythnet-price-feeds"],
+  [
+    "/documentation/pythnet-price-feeds/on-demand",
+    "/price-feeds/pythnet-price-feeds/pull-updates",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/best-practices",
+    "/price-feeds/pythnet-price-feeds/best-practices",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/hermes",
+    "/price-feeds/pythnet-price-feeds/hermes",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/scheduler",
+    "/price-feeds/schedule-price-updates/using-scheduler",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/aptos",
+    "/price-feeds/use-real-time-data/aptos",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/cosmwasm",
+    "/price-feeds/use-real-time-data/cosmwasm",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/sui",
+    "/price-feeds/use-real-time-data/sui",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/near",
+    "/price-feeds/use-real-time-data/near",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/off-chain",
+    "/price-feeds/use-real-time-data/off-chain",
+  ],
+  [
+    "/documentation/pythnet-price-feeds/evm",
+    "/price-feeds/use-real-time-data/evm",
+  ],
+
+  ["/documentation", "/home"],
+
+  ["/api-explorer/:slug*", "/price-feeds/api-reference/:slug*"],
+
+  [
+    "/guides/how-to-schedule-price-updates-with-gelato",
+    "/price-feeds/schedule-price-updates/using-gelato",
+  ],
+  [
+    "/guides/how-to-create-tradingview-charts",
+    "/benchmarks/how-to-create-tradingview-charts",
+  ],
+  ["/guides", "/price-feeds"],
+];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -17,28 +110,13 @@ const nextConfig = {
     return config;
   },
   async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
+    return permanentRedirectArray.map((value) => {
+      return {
+        source: value[0],
+        destination: value[1],
         permanent: true,
-      },
-      {
-        source: "/evm",
-        destination: "/price-feeds/api-reference/evm",
-        permanent: true,
-      },
-      {
-        source: "/aptos",
-        destination: "/price-feeds/api-reference/aptos",
-        permanent: true,
-      },
-      {
-        source: "/cosmwasm",
-        destination: "/price-feeds/api-reference/cosmwasm",
-        permanent: true,
-      },
-    ];
+      };
+    });
   },
 };
 
