b2538227081a7ab1989c13f749d243de0d0570a3
Connor Prussin <connor@prussin.net>
2024-06-25 12:59:49 -0700

    Fix amplitude tracking (#355)
    - Don't double count in dev
    - Don't warn about default tracking
    - Fix button click events

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { GrazProvider } from "graz";
 import { AppProps } from "next/app";
 import Script from "next/script";
 import "nextra-theme-docs/style.css";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState, useRef } from "react";
 import { WagmiConfig, createConfig } from "wagmi";
 import { arbitrum, avalanche, mainnet, sepolia } from "wagmi/chains";
 import * as amplitude from "@amplitude/analytics-browser";
@@ -37,17 +37,21 @@ const config = createConfig(
 
 export default function Nextra({ Component, pageProps }: NextraAppProps) {
   const [mounted, setMounted] = useState(false);
+  const amplitudeInitialized = useRef(false);
 
   useEffect(() => setMounted(true), []);
   useEffect(() => {
     TagManager.initialize(tagManagerArgs);
   }, []);
   useEffect(() => {
-    if (AMPLITUDE_API_KEY) {
-      amplitude.init(AMPLITUDE_API_KEY);
+    if (AMPLITUDE_API_KEY && !amplitudeInitialized.current) {
       amplitude.add(autocapturePlugin());
+      amplitude.init(AMPLITUDE_API_KEY, {
+        defaultTracking: true,
+      });
+      amplitudeInitialized.current = true;
     }
-  });
+  }, []);
 
   // Make the global context available to every page.
   return (
