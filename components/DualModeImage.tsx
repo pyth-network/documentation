89372da53af32903d0ca83720693cb07a47545c1
Aditya Arora <arora.aditya520@gmail.com>
2024-10-29 22:52:27 -0400

    final fix

--- components/DualModeImage.tsx
+++ components/DualModeImage.tsx
@@ -1,19 +1,34 @@
 import type { ComponentProps } from "react";
 import Image from "next/image";
 
-
 type ImageProps = ComponentProps<typeof Image>;
 type Props = Omit<ImageProps, "src"> & {
   darkSrc: ImageProps["src"];
   lightSrc: ImageProps["src"];
   alt: string;
-}
+};
 
-const DualModeImage = ({ darkSrc, lightSrc, className, alt, ...props }: Props) => {
+const DualModeImage = ({
+  darkSrc,
+  lightSrc,
+  className,
+  alt,
+  ...props
+}: Props) => {
   return (
     <>
-      <Image src={lightSrc} className={`dark:hidden ${className}`} alt={alt} {...props} />
-      <Image src={darkSrc} className={`hidden dark:block ${className}`} alt={alt} {...props} />
+      <Image
+        src={lightSrc}
+        className={`dark:hidden ${className}`}
+        alt={alt}
+        {...props}
+      />
+      <Image
+        src={darkSrc}
+        className={`hidden dark:block ${className}`}
+        alt={alt}
+        {...props}
+      />
     </>
   );
 };
