17f9b750b96e2f2fd585b4772793a1acf8446cfb
Aditya Arora <arora.aditya520@gmail.com>
2024-11-12 14:55:55 +0700

    resolved comments

--- components/PriceFeedTable.tsx
+++ components/PriceFeedTable.tsx
@@ -1,105 +1,94 @@
 import { useState, useEffect } from "react";
-import { TextField, Select, MenuItem, Box } from "@mui/material";
-import { HermesClient } from "@pythnetwork/hermes-client";
+import { TextField, Select, MenuItem } from "@mui/material";
+import { HermesClient, PriceFeedMetadata } from "@pythnetwork/hermes-client";
 import { Table, Td, Th, Tr, CopyToClipboard } from "nextra/components";
 
-interface PriceFeed {
-  id: string;
-  name: string;
-  assetType: string;
+const fetchStablePriceFeeds = async () => {
+  const priceFeeds = await new HermesClient(
+    "https://hermes.pyth.network"
+  ).getPriceFeeds();
+  const assetTypes = Array.from(
+    new Set(priceFeeds.map((feed) => feed.attributes.asset_type ?? ""))
+  );
+  console.log(priceFeeds);
+  return { priceFeeds, assetTypes };
+};
+
+const fetchBetaPriceFeeds = async () => {
+  const priceFeeds = await new HermesClient(
+    "https://hermes-beta.pyth.network"
+  ).getPriceFeeds();
+  const assetTypes = Array.from(
+    new Set(priceFeeds.map((feed) => feed.attributes.asset_type ?? ""))
+  );
+  console.log(priceFeeds);
+  return { priceFeeds, assetTypes };
+};
+
+type AssetTypesSelectorProps = {
+  priceFeedsState: PriceFeedsState;
+  selectedAssetType: string;
+  setSelectedAssetType: (assetType: string) => void;
+};
+
+enum PriceFeedsStateType {
+  NotLoaded,
+  Loading,
+  Loaded,
+  Error,
 }
 
-export function PriceFeedTable() {
-  const [priceFeeds, setPriceFeeds] = useState<PriceFeed[]>([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedAssetType, setSelectedAssetType] = useState("All");
+const PriceFeedsState = {
+  NotLoaded: () => ({ type: PriceFeedsStateType.NotLoaded as const }),
+  Loading: () => ({ type: PriceFeedsStateType.Loading as const }),
+  Loaded: (priceFeeds: PriceFeedMetadata[], assetTypes: string[]) => ({
+    type: PriceFeedsStateType.Loaded as const,
+    priceFeeds,
+    assetTypes,
+  }),
+  Error: (error: unknown) => ({
+    type: PriceFeedsStateType.Error as const,
+    error,
+  }),
+};
 
-  // enum AssetTypesStateType {
-  //   NotLoaded,
-  //   Loading,
-  //   Loaded,
-  //   Error,
-  // }
+type PriceFeedsState = ReturnType<
+  typeof PriceFeedsState[keyof typeof PriceFeedsState]
+>;
 
-  // const AssetTypesState = {
-  //   NotLoaded: () => ({ type: AssetTypesStateType.NotLoaded as const }),
-  //   Loading: () => ({ type: AssetTypesStateType.Loading as const }),
-  //   Loaded: (assetTypes: string[]) => ({
-  //     type: AssetTypesStateType.Loaded as const,
-  //     assetTypes,
-  //   }),
-  //   Error: (error: unknown) => ({
-  //     type: AssetTypesStateType.Error as const,
-  //     error,
-  //   }),
-  // };
-
-  const fetchAssetTypes = async () => {
-    const hermesClient = new HermesClient("https://hermes.pyth.network");
-    const feeds = await hermesClient.getPriceFeeds();
-    return Array.from(
-      new Set(feeds.map((feed) => feed.attributes.asset_type ?? ""))
-    );
-  };
-
-  // const useAssetTypes = (): typeof AssetTypesState => {
-  //   const [assetTypes, setAssetTypes] = useState<typeof AssetTypesState>(
-  //     AssetTypesState.NotLoaded()
-  //   );
-
-  //   useEffect(() => {
-  //     setAssetTypes(AssetTypesState.Loading());
-  //   }, []);
-
-  //   return assetTypes;
-  // };
-
-  // type AssetTypesSelectorProps = {
-  //   selectedAssetType: string;
-  //   setSelectedAssetType: (assetType: string) => void;
-  // };
-
-  // const AssetTypesSelector = ({
-  //   selectedAssetType,
-  //   setSelectedAssetType,
-  // }: AssetTypesSelectorProps) => {
-  //   const assetTypes = useAssetTypes();
-  // };
+const usePriceFeeds = (): PriceFeedsState => {
+  const [priceFeeds, setPriceFeeds] = useState<PriceFeedsState>(
+    PriceFeedsState.NotLoaded()
+  );
 
   useEffect(() => {
-    const fetchPriceFeeds = async () => {
-      const hermesClient = new HermesClient("https://hermes.pyth.network");
-      const feeds = await hermesClient.getPriceFeeds();
-      const transformedFeeds = feeds.map((feed) => ({
-        id: feed.id,
-        name: feed.attributes.display_symbol || "",
-        assetType: feed.attributes.asset_type || "",
-      }));
-      setPriceFeeds(transformedFeeds);
-    };
-
-    fetchPriceFeeds();
+    setPriceFeeds(PriceFeedsState.Loading());
+    fetchStablePriceFeeds()
+      .then(({ priceFeeds, assetTypes }) => {
+        setPriceFeeds(PriceFeedsState.Loaded(priceFeeds, assetTypes));
+      })
+      .catch((error) => {
+        setPriceFeeds(PriceFeedsState.Error(error));
+      });
   }, []);
 
-  const filteredData = priceFeeds.filter((feed) => {
-    const matchesSearch =
-      feed.id.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      feed.name.toLowerCase().includes(searchTerm.toLowerCase());
-    const matchesAssetType =
-      selectedAssetType === "All" || feed.assetType === selectedAssetType;
-    return matchesSearch && matchesAssetType;
-  });
-  return (
-    <Box>
-      <Box sx={{ mb: 3, display: "flex", gap: 2 }}>
-        <TextField
-          label="Search price feeds"
-          variant="outlined"
-          size="small"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          sx={{ width: 300 }}
-        />
+  return priceFeeds;
+};
+
+const AssetTypesSelect = ({
+  priceFeedsState,
+  selectedAssetType,
+  setSelectedAssetType,
+}: AssetTypesSelectorProps) => {
+  const priceFeeds = usePriceFeeds();
+
+  switch (priceFeeds.type) {
+    case PriceFeedsStateType.NotLoaded:
+    case PriceFeedsStateType.Loading:
+    case PriceFeedsStateType.Error:
+      return <p>Error loading asset types</p>;
+    case PriceFeedsStateType.Loaded:
+      return (
         <Select
           value={selectedAssetType}
           onChange={(e) => setSelectedAssetType(e.target.value)}
@@ -107,15 +96,66 @@ export function PriceFeedTable() {
           sx={{ width: 200 }}
         >
           <MenuItem value="All">All Asset Types</MenuItem>
-          {Array.from(new Set(priceFeeds.map((feed) => feed.assetType))).map(
-            (type) => (
-              <MenuItem key={type} value={type}>
-                {type || "Uncategorized"}
-              </MenuItem>
-            )
-          )}
+          {priceFeeds.assetTypes.map((type) => (
+            <MenuItem key={type} value={type}>
+              {type ?? "Uncategorized"}
+            </MenuItem>
+          ))}
         </Select>
-      </Box>
+      );
+  }
+};
+
+export function PriceFeedTable() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedAssetType, setSelectedAssetType] = useState("All");
+  const priceFeedsState = usePriceFeeds();
+
+  <AssetTypesSelect
+    priceFeedsState={priceFeedsState}
+    selectedAssetType={selectedAssetType}
+    setSelectedAssetType={setSelectedAssetType}
+  />;
+
+  let filteredData: PriceFeedMetadata[] = [];
+  if (priceFeedsState.type === PriceFeedsStateType.Loaded) {
+    filteredData = priceFeedsState.priceFeeds.filter((feed) => {
+      const matchesSearch =
+        feed.id.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        feed.attributes.display_symbol
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      const matchesAssetType =
+        selectedAssetType === "All" ||
+        feed.attributes.asset_type === selectedAssetType;
+      return matchesSearch && matchesAssetType;
+    });
+  }
+
+  return (
+    <>
+      <TextField
+        label="Search price feeds"
+        variant="outlined"
+        size="small"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        sx={{ width: 300 }}
+      />
+      <Select
+        value={selectedAssetType}
+        onChange={(e) => setSelectedAssetType(e.target.value)}
+        size="small"
+        sx={{ width: 200 }}
+      >
+        <MenuItem value="All">All Asset Types</MenuItem>
+        {priceFeedsState.type === PriceFeedsStateType.Loaded &&
+          priceFeedsState.assetTypes.map((type) => (
+            <MenuItem key={type} value={type}>
+              {type || "Uncategorized"}
+            </MenuItem>
+          ))}
+      </Select>
 
       <Table>
         <thead>
@@ -128,16 +168,16 @@ export function PriceFeedTable() {
         <tbody>
           {filteredData.map((feed) => (
             <Tr key={feed.id}>
-              <Td>{feed.name}</Td>
-              <Td>{feed.assetType}</Td>
-              <Td>
+              <Td>{feed.attributes.display_symbol}</Td>
+              <Td>{feed.attributes.asset_type}</Td>
+              <Td className="font-mono whitespace-nowrap">
                 {feed.id}
-                <CopyToClipboard getValue={() => feed.id} />
+                <CopyToClipboard className="ml-2" getValue={() => feed.id} />
               </Td>
             </Tr>
           ))}
         </tbody>
       </Table>
-    </Box>
+    </>
   );
 }
