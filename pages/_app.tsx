ee2c90c791ce579568d94e934e68859205f1d463
Connor Prussin <connor@prussin.net>
2025-02-18 12:05:12 -0800

    fix: re-enable SSR
    To be quite honest, I'm not sure if this is safe or what it might break (or why
    this was ever done in the first place).  However, having SSR disabled is causing
    some docs pages to have terrible performance and people are complaining.
    
    I did some testing with this and I didn't see any issues.  At this point I
    believe the best path forward is to re-enable SSR and just fix forward if we
    find any issues I missed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { GrazProvider } from "graz";
 import { AppProps } from "next/app";
 import Script from "next/script";
 import "nextra-theme-docs/style.css";
-import { ReactNode, useEffect, useState, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import { WagmiConfig, createConfig } from "wagmi";
 import { arbitrum, avalanche, mainnet, sepolia } from "wagmi/chains";
 import * as amplitude from "@amplitude/analytics-browser";
@@ -37,10 +37,8 @@ const config = createConfig(
 );
 
 export default function Nextra({ Component, pageProps }: NextraAppProps) {
-  const [mounted, setMounted] = useState(false);
   const amplitudeInitialized = useRef(false);
 
-  useEffect(() => setMounted(true), []);
   useEffect(() => {
     TagManager.initialize(tagManagerArgs);
   }, []);
@@ -54,44 +52,40 @@ export default function Nextra({ Component, pageProps }: NextraAppProps) {
     }
   }, []);
 
-  // Make the global context available to every page.
   return (
-    // prevent react hydration error
-    mounted && (
-      <>
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-7TVVW3MEK7"
-        />
-        <Script id="google-tag">
-          {`
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-7TVVW3MEK7"
+      />
+      <Script id="google-tag">
+        {`
+       window.dataLayer = window.dataLayer || [];
+       function gtag(){dataLayer.push(arguments);}
+       gtag('js', new Date());
 
-         gtag('config', 'G-7TVVW3MEK7');
-       `}
-        </Script>
-        <AskCookbook />
-        <GrazProvider grazOptions={{ chains: CosmosChains }}>
-          <WagmiConfig config={config}>
-            <ConnectKitProvider
-              theme="midnight"
-              customTheme={{
-                "--ck-connectbutton-background": "#E6DAFE",
-                "--ck-connectbutton-hover-background": "#F2ECFF",
-                "--ck-connectbutton-color": "#141227",
-                "--ck-font-family": "Red Hat Text, sans-serif",
-                "--ck-connectbutton-font-size": "14px",
-              }}
-            >
-              <GlobalContextProvider>
-                <Component {...pageProps} />
-              </GlobalContextProvider>
-            </ConnectKitProvider>
-          </WagmiConfig>
-        </GrazProvider>
-      </>
-    )
+       gtag('config', 'G-7TVVW3MEK7');
+     `}
+      </Script>
+      <AskCookbook />
+      <GrazProvider grazOptions={{ chains: CosmosChains }}>
+        <WagmiConfig config={config}>
+          <ConnectKitProvider
+            theme="midnight"
+            customTheme={{
+              "--ck-connectbutton-background": "#E6DAFE",
+              "--ck-connectbutton-hover-background": "#F2ECFF",
+              "--ck-connectbutton-color": "#141227",
+              "--ck-font-family": "Red Hat Text, sans-serif",
+              "--ck-connectbutton-font-size": "14px",
+            }}
+          >
+            <GlobalContextProvider>
+              <Component {...pageProps} />
+            </GlobalContextProvider>
+          </ConnectKitProvider>
+        </WagmiConfig>
+      </GrazProvider>
+    </>
   );
 }
