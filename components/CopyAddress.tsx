99b6a234f5a5951085db484f5e96398bf7fa2ba4
Connor Prussin <connor@prussin.net>
2025-07-28 17:58:26 -0700

    Add price feed ids table

--- components/CopyAddress.tsx
+++ components/CopyAddress.tsx
@@ -1,18 +1,27 @@
 import CopyButton from "./CopyButton";
+import clsx from "clsx";
 
-const CopyAddress = ({ address, url }: { address: string; url?: string }) => {
+type Props = {
+  address: string;
+  url?: string;
+  alwaysTruncate?: boolean | undefined;
+};
+
+const CopyAddress = ({ address, url, alwaysTruncate }: Props) => {
   return (
     <CopyButton value={address} className="-ml-1">
-      <span className="mr-2 hidden lg:block">
-        {url ? (
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            {address}
-          </a>
-        ) : (
-          address
-        )}
-      </span>
-      <span className="mr-2 lg:hidden">
+      {!alwaysTruncate && (
+        <span className="mr-2 hidden lg:block">
+          {url ? (
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              {address}
+            </a>
+          ) : (
+            address
+          )}
+        </span>
+      )}
+      <span className={clsx("mr-2", { "lg:hidden": !alwaysTruncate })}>
         {url ? (
           <a href={url} target="_blank" rel="noopener noreferrer">
             {address.slice(0, 6) + "..." + address.slice(-6)}
