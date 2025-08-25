c7c873f3b3b94adfdf23811745dbe9211cbafbcd
Aditya Arora <arora.aditya520@gmail.com>
2025-08-25 12:05:44 -0400

    requested changes

--- components/icons/ProductIcons.tsx
+++ components/icons/ProductIcons.tsx
@@ -1,127 +1,43 @@
 import React from "react";
+import {
+  ChartBar,
+  Shield,
+  Code,
+  Lightning,
+  Globe,
+  FileText,
+  Clock,
+  ArrowsOutCardinal,
+} from "@phosphor-icons/react";
 
 export function BarChartIcon() {
-  return (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-      <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
-    </svg>
-  );
+  return <ChartBar className="w-5 h-5" weight="regular" />;
 }
 
 export function ShieldIcon() {
-  return (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        fillRule="evenodd"
-        d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  return <Shield className="w-5 h-5" weight="regular" />;
 }
 
 export function CodeIcon() {
-  return (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        fillRule="evenodd"
-        d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  return <Code className="w-5 h-5" weight="regular" />;
 }
 
 export function LightningIcon() {
-  return (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        fillRule="evenodd"
-        d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  return <Lightning className="w-5 h-5" weight="regular" />;
 }
 
 export function GlobeIcon() {
-  return (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        fillRule="evenodd"
-        d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.5A1.5 1.5 0 019.5 17h-1A1.5 1.5 0 017 15.5V15a2 2 0 00-2-2H4.332z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  return <Globe className="w-5 h-5" weight="regular" />;
 }
 
 export function DocumentIcon() {
-  return (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        fillRule="evenodd"
-        d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  return <FileText className="w-5 h-5" weight="regular" />;
 }
 
 export function ClockIcon() {
-  return (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-      <path
-        fillRule="evenodd"
-        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  return <Clock className="w-5 h-5" weight="regular" />;
 }
 
 export function MultiChainIcon() {
-  return (
-    <svg
-      className="w-5 h-5"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-    >
-      {/* Three interconnected block-like shapes */}
-      <rect
-        x="4"
-        y="8"
-        width="4"
-        height="4"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <rect
-        x="16"
-        y="8"
-        width="4"
-        height="4"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <rect
-        x="10"
-        y="16"
-        width="4"
-        height="4"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-
-      {/* Chain-link connections between blocks */}
-      <path d="M8 10 L16 10" strokeWidth="2" strokeLinecap="round" />
-      <path d="M12 12 L12 16" strokeWidth="2" strokeLinecap="round" />
-      <path d="M8 10 L12 12" strokeWidth="2" strokeLinecap="round" />
-      <path d="M16 10 L12 12" strokeWidth="2" strokeLinecap="round" />
-    </svg>
-  );
+  return <ArrowsOutCardinal className="w-5 h-5" weight="regular" />;
 }
